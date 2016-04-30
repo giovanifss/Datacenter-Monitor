4db1c558a880c473f227a512d0c4d6db34cb0e07
Giovani Ferreira <silva.giovanif@gmail.com>
2016-04-30 12:24:04 -0300

    Update setup.sh
    Fix name problems

--- setup.sh
+++ setup.sh
@@ -9,13 +9,13 @@ echo "$(tput setaf 1)==> Downloading library$(tput sgr 0)"
 git clone git@github.com:adafruit/DHT-sensor-library.git
 
 echo "$(tput setaf 1)==> Copying code files$(tput sgr 0)"
-mkdir DHT-sensor
-cp DHT-sensor-library/DHT.cpp DHT-sensor-library/DHT.h DHT-sensor/
+mkdir DHT
+cp DHT-sensor-library/DHT.cpp DHT-sensor-library/DHT.h DHT/
 
 echo "$(tput setaf 1)==> Removing files$(tput sgr 0)"
 sudo rm -r DHT-sensor-library
 
 echo "$(tput setaf 1)==> Instaling library$(tput sgr 0)"
-sudo mv DHT-sensor /usr/share/arduino/libraries/
+sudo mv DHT/ /usr/share/arduino/libraries/
 
 echo "$(tput setaf 1)==> Successfully installed library$(tput sgr 0)"
