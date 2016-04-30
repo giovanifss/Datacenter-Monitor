aa458960f93b205206ec7bd91193d4db95b70903
Giovani Ferreira <silva.giovanif@gmail.com>
2016-04-30 12:21:41 -0300

    Added basic script to setup libraries needed to execute the project

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "::Error: Please run as root"
+  exit
+fi
+
+echo "$(tput setaf 1)==> Downloading library$(tput sgr 0)"
+git clone git@github.com:adafruit/DHT-sensor-library.git
+
+echo "$(tput setaf 1)==> Copying code files$(tput sgr 0)"
+mkdir DHT-sensor
+cp DHT-sensor-library/DHT.cpp DHT-sensor-library/DHT.h DHT-sensor/
+
+echo "$(tput setaf 1)==> Removing files$(tput sgr 0)"
+sudo rm -r DHT-sensor-library
+
+echo "$(tput setaf 1)==> Instaling library$(tput sgr 0)"
+sudo mv DHT-sensor /usr/share/arduino/libraries/
+
+echo "$(tput setaf 1)==> Successfully installed library$(tput sgr 0)"
