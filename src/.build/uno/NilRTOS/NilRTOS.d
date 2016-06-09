603f4bd5acbe984152a60cd7744fbc1a8530b0ff
Giovani Ferreira <silva.giovanif@gmail.com>
2016-06-08 22:25:32 -0300

    Added git ignore and set to ignore builds. Removed builds in repository.

--- src/.build/uno/NilRTOS/NilRTOS.d
+++
@@ -1,7 +0,0 @@
-.build/uno/NilRTOS/NilRTOS.d .build/uno/NilRTOS/NilRTOS.o: /usr/share/arduino/libraries/NilRTOS/NilRTOS.c \
-  /usr/share/arduino/libraries/NilRTOS/NilRTOS.h \
-  /usr/share/arduino/libraries/NilRTOS/nil.h \
-  /usr/share/arduino/libraries/NilRTOS/nilconf.h \
-  /usr/share/arduino/libraries/NilRTOS/niltypes.h \
-  /usr/share/arduino/libraries/NilRTOS/nilcore.h \
-  /usr/share/arduino/libraries/NilRTOS/avr_heap.h
--- src/.build/uno/NilRTOS/board.d
+++
@@ -1,5 +0,0 @@
-.build/uno/NilRTOS/board.d .build/uno/NilRTOS/board.o: /usr/share/arduino/libraries/NilRTOS/board.c \
-  /usr/share/arduino/libraries/NilRTOS/nil.h \
-  /usr/share/arduino/libraries/NilRTOS/nilconf.h \
-  /usr/share/arduino/libraries/NilRTOS/niltypes.h \
-  /usr/share/arduino/libraries/NilRTOS/nilcore.h
